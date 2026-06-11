6bec9f4a65152bec0070e42901f381fe4960f4a0
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2026-06-11 22:07:28 +0300

    weak import
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- libs/musl/src/polkavm/polkavm.c
+++ libs/musl/src/polkavm/polkavm.c
@@ -2,6 +2,10 @@
 #include "locale_impl.h"
 #include "pthread_impl.h"
 #include "pthread_arch.h"
+#include "polkavm_guest.h"
+
+// Weak default, overridden by a strong pvm_syscall if the program provides one.
+POLKAVM_IMPORT_WEAK(long, pvm_syscall, long, long, long, long, long, long, long)
 
 long _syscall_polkavm(long n, long a, long b, long c, long d, long e, long f)
 {
